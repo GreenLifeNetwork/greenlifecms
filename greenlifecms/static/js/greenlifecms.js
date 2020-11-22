e6c3d8dd891a871084c68b93b29b69fcf3a13188
Yann VR <yannvr@gmail.com>
2020-11-22 22:36:39 +0000

    feat(js) bypass mobile web promo for native app

--- greenlifecms/static/js/greenlifecms.js
+++ greenlifecms/static/js/greenlifecms.js
@@ -1,45 +1,41 @@
+const isNativeApp = window.isNativeApp || location.search.indexOf("isNativeApp") > -1
 const GreenLifeApp = () => {
-    const md           = new MobileDetect(window.navigator.userAgent);
-    const ios_url      = "https://apps.apple.com/us/app/green-lifestyle/id1484892517";
-    const android_url  = "https://play.google.com/store/apps/details?id=lastchance.greenlife.quotes";
-    const fallback_url = "https://greenlife.cloud/";
-    let isDesktop = false;
-    let isAndroid = false;
-    let isIOS = false;
-    if (md.os() == "iOS") {
-        isIOS = true
-    } else if (md.os() == "AndroidOS") {
-        isAndroid = true
-    } else {
-        isDesktop = true
+  const md = new MobileDetect(window.navigator.userAgent)
+  const ios_url = "https://apps.apple.com/us/app/green-lifestyle/id1484892517"
+  const android_url = "https://play.google.com/store/apps/details?id=lastchance.greenlife.quotes"
+  let isDesktop = false
+  let isAndroid = false
+  let isIOS = false
+  if (md.os() === "iOS") {
+    isIOS = true
+  } else if (md.os() === "AndroidOS") {
+    isAndroid = true
+  } else {
+    isDesktop = true
+  }
+
+  const overrideLinksWithAppStoreLink = () => {
+    const articleLinks = document.querySelectorAll("article a")
+    articleLinks.forEach(link => {
+      link.href = isIOS ? ios_url : android_url
+    })
+  }
+
+  if (isIOS || isAndroid) {
+    overrideLinksWithAppStoreLink()
+    if (isIOS) {
+      document.querySelector("#ios-app-available").style.display = "block"
     }
-
-
-    const isApp = location.search.indexOf('format=app') > -1
-    const overrideLinksWithAppStoreLink = () => {
-        const articleLinks = document.querySelectorAll('article a')
-        articleLinks.forEach(link => {
-            link.href = isIOS ? ios_url : android_url
-        })
+    if (isAndroid) {
+      document.querySelector("#android-app-available").style.display = "block"
     }
-
-    if(isApp) {
-        document.querySelector('#nudge-links').style.display = 'none'
-        setTimeout(() => document.querySelector('footer').style.display = 'none')
-        console.log('isApp')
-    } else {
-        if (isIOS || isAndroid) {
-            overrideLinksWithAppStoreLink()
-            if(isIOS) {
-                document.querySelector('#ios-app-available').style.display = 'block'
-            }
-            if(isAndroid) {
-                document.querySelector('#android-app-available').style.display = 'block'
-            }
-        }
-    }
-
-
+  }
 }
 
-GreenLifeApp()
+if (!isNativeApp) {
+  GreenLifeApp()
+} else {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector("footer").style.display = "none"
+  })
+}
