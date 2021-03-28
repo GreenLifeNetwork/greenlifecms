0bd5caebd42dc9c2c5403ac20bcfeb9fcb1f89f9
Yann VR <yannvr@coin-advisor.co>
2021-03-28 23:33:10 +0000

    feat(pm2) +ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+            name: 'greenlife-cms',
+            script: 'manage.py',
+            interpreter: '/home/ubuntu/.pyenv/versions/3.7.4/bin/python',
+            args: 'runserver'
+    }, ],
+};
